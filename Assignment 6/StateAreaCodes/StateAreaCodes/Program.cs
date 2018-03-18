2a6b70ddb3faa6b5cc98ea00a41543476f7b783b
Cole Hemp <ColeHemp@gmail.com>
2018-03-18 18:27:34 -0400

    Added Assignment 6
    I've completed the work for Assignment 6, StateAreaCodes, and added it to the repository.

---
+++ Assignment 6/StateAreaCodes/StateAreaCodes/Program.cs
@@ -0,0 +1,41 @@
+﻿//Cole Hemp
+//COP2360
+//3/18/18
+//Program.cs
+//A class dedicated to testing the functionality of StateAreaCodes.CS
+
+using System;
+
+namespace StateAreaCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stateInitials = "FL";
+            int[] areaCodes = new int[] { 305, 321, 352, 386, 407, 561, 727, 754, 772, 786, 813, 850, 863, 904, 941, 954 };
+            int userInput;
+            bool checkResults;
+
+            StateAreaCodes testStateAreaCodes = new StateAreaCodes(stateInitials, areaCodes);
+
+            Console.Write("Welcome to the Florida Area Code Check!\nInput an area code: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            checkResults = testStateAreaCodes.CheckForAreaCode(userInput);
+
+            if (checkResults == true)
+            {
+                Console.WriteLine("\nCongrats! " + userInput + " is an area code in Florida.");
+                Console.Write("All of Florida's area codes. \n" + testStateAreaCodes.ToString());
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nSorry, " + userInput + " is not an area code in Florida.");
+                Console.Write("All of Florida's area codes. \n" + testStateAreaCodes.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Assignment 6/StateAreaCodes/StateAreaCodes/StateAreaCodes.cs
@@ -0,0 +1,56 @@
+﻿//Cole Hemp
+//COP2360
+//3/18/18
+//StateAreaCodes.cs
+//A class for checking and managing state area codes
+
+namespace StateAreaCodes
+{
+    class StateAreaCodes
+    {
+        //Properties
+        public string StateInitials { get; set; }
+        public int[] AreaCodes { get; set; }
+
+        //Default Constructor
+        public StateAreaCodes()
+        {
+
+        }
+        //Overloaded Constructor
+        public StateAreaCodes(string stateInitials, int[] areaCodes)
+        {
+            this.AreaCodes = areaCodes;
+            this.StateInitials = stateInitials;
+        }
+
+        //Checks if user's inputed area code is in the array
+        public bool CheckForAreaCode(int userInput)
+        {
+            for(int i = 0; i < AreaCodes.Length; i++)
+            {
+                if(userInput == AreaCodes[i])
+                {
+                    //Returns true if there is a match
+                    return true;
+                }
+            }
+
+            //If no match happens in the for loop, returns false
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string outputString;
+
+            outputString = StateInitials + ": ";
+
+            for(int i = 0; i < AreaCodes.Length; i++)
+            {
+                outputString = outputString + "(" + AreaCodes[i] + ") ";
+            }
+            return outputString;
+        }
+    }
+}
